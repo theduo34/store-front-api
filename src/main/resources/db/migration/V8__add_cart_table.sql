f59b0f1b8e25e9232eeb2bb9217cde571320562e
Emmanuel <emmsom506@gmail.com>
2025-09-22 09:00:05 +0000

    Add table for cart items

---
+++ src/main/resources/db/migration/V8__add_cart_table.sql
@@ -0,0 +1,12 @@
+create table cart_items
+(
+    id bigint auto_increment primary key,
+    cart_id    binary(16)    not null,
+    product_id bigint        not null,
+    quantity   int default 1 not null,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    constraint cart_items_carts_products_unique unique (cart_id, product_id),
+    constraint cart_items_carts_id_fk foreign key (cart_id) references carts (id) on delete cascade,
+    constraint cart_items_products_id_fk foreign key (product_id) references products (id) on delete cascade
+);

20222e23d7b0f028b38560c6c0eb4710ff7468d4
Emmanuel <emmsom506@gmail.com>
2025-09-24 13:05:27 +0000

    Add jwt response and login request dto class

---
+++ src/main/java/com/theduo/storefront/auth/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.theduo.storefront.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 25, message = "Password must be between 6 and 25 character long")
+    private String password;
+
+}

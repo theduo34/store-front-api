359ff6db74354ca0815d6d276f966cdf812455ce
Emmanuel <emmsom506@gmail.com>
2025-09-24 13:06:59 +0000

    Add jwt and jwt service class

---
+++ src/main/java/com/theduo/storefront/common/util/JwtService.java
@@ -0,0 +1,56 @@
+package com.theduo.storefront.common.util;
+
+import com.theduo.storefront.common.config.JwtConfig;
+import com.theduo.storefront.user.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@Service
+public class JwtService {
+    private final JwtConfig jwtConfig;
+
+    public Jwt generateAccessToken(User user) {
+        return generateToken(user, jwtConfig.getAccessTokenExpiration());
+    }
+
+    public Jwt generateRefreshToken(User user) {
+        return generateToken(user,  jwtConfig.getRefreshTokenExpiration());
+    }
+
+    private Jwt generateToken(User user, long tokenExpiration) {
+        var claims = Jwts.claims()
+                .subject(user.getId().toString())
+                .add("name", user.getFirstName() + user.getLastName())
+                .add("email", user.getEmail())
+                .add("role", user.getGroupType().getRole().toString())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
+                .build();
+
+        return new Jwt(claims, jwtConfig.getSecretKey());
+    }
+
+    public Jwt parseToken(String token) {
+        try{
+            var claims = getClaims(token);
+            return new Jwt(claims, jwtConfig.getSecretKey());
+        } catch (JwtException e) {
+            return null;
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(jwtConfig.getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+}

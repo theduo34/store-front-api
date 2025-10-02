7c0a5828a8851d3ea77ed99dcc8f09307ba18a52
Emmanuel <emmsom506@gmail.com>
2025-10-02 08:15:36 +0000

    Add product exception

---
+++ src/main/java/com/theduo/storefront/product/exception/ProductNotException.java
@@ -0,0 +1,7 @@
+package com.theduo.storefront.product.exception;
+
+public class ProductNotException extends RuntimeException {
+    public ProductNotException(String message) {
+        super(message);
+    }
+}

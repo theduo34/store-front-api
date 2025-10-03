a8a21a31cf8d87c2410159568b1453882541f5da
Emmanuel <emmsom506@gmail.com>
2025-10-03 08:34:23 +0000

    Fix categoryId null issue

--- src/main/java/com/theduo/storefront/product/mapper/ProductMapper.java
+++ src/main/java/com/theduo/storefront/product/mapper/ProductMapper.java
@@ -4,9 +4,12 @@ import com.theduo.storefront.product.dto.CreateProductRequest;
 import com.theduo.storefront.product.dto.ProductDto;
 import com.theduo.storefront.product.entity.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+    @Mapping(source = "category.id", target = "categoryId")
     ProductDto toProductDto(Product product);
+
     Product toRegisterDto(CreateProductRequest request);
 }

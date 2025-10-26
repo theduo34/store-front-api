7f39fe7be0ac0d994af69656619799aca81d7a5b
Emmanuel <emmsom506@gmail.com>
2025-10-26 14:29:43 +0000

    Add to cartItemDto method to the cart mapper

--- src/main/java/com/theduo/storefront/cart/mapper/CartMapper.java
+++ src/main/java/com/theduo/storefront/cart/mapper/CartMapper.java
@@ -1,7 +1,9 @@
 package com.theduo.storefront.cart.mapper;
 
 import com.theduo.storefront.cart.dto.CartDto;
+import com.theduo.storefront.cart.dto.CartItemDto;
 import com.theduo.storefront.cart.entity.Cart;
+import com.theduo.storefront.cart.entity.CartItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -10,4 +12,7 @@ public interface CartMapper {
     @Mapping(target="items", source="items")
     @Mapping(target="totalPrice", expression = "java(cart.getTotalPrice())")
     CartDto toCartDto(Cart cart);
+
+    @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
+    CartItemDto toCartItemDto(CartItem cartItem);
 }
